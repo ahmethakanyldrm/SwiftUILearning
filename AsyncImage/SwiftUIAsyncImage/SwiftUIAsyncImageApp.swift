5844223bdbedea2132b6de3204d59d2aeab0d6ad
ahmethakanyldrm <65174957+ahmethakanyldrm@users.noreply.github.com>
2023-01-02 22:01:55 +0300

    First Project AsyncImage

---
+++ AsyncImage/SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,12 @@
+//  Created by AHMET HAKAN YILDIRIM on 02.01.2023
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
