7ecdba9b1259fb8d51025a411ed1d2d905ccba27
ahmethakanyldrm <65174957+ahmethakanyldrm@users.noreply.github.com>
2023-01-06 13:22:09 +0300

    State and HStack and rawValue and Identifiable written in Slot machine app

---
+++ slotMachine/slotMachine/SwiftUIView.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUIView.swift
+//  slotMachine
+//
+//  Created by AHMET HAKAN YILDIRIM on 5.01.2023.
+//
+
+import SwiftUI
+
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        Hexagonal()
+            .frame(width: 100, height: 120,alignment: .center)
+    }
+}
---
+++ slotMachine/slotMachine/slotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  slotMachineApp.swift
+//  slotMachine
+//
+//  Created by AHMET HAKAN YILDIRIM on 5.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct slotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
